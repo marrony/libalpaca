9a43d2113737b713b468a74b39eea45b92174bb7
Marrony Neris <neris.marrony@gmail.com>
2023-08-14 10:38:15 -0700

    change alpaca_error definition

--- include/errors.hpp
+++ include/errors.hpp
@@ -10,63 +10,57 @@ namespace alpaca {
 struct alpaca_error {
   const int error_number;
   const std::string error_message;
-
-  alpaca_error(int error_number, const std::string& error_message)
-  : error_number(error_number)
-  , error_message(error_message)
-  { }
 };
 
 // reserved error code (0x400) for property or method not implemented.
 auto not_implemented() {
-  return alpaca_error(0x0400, "Not implemented");
+  return alpaca_error{0x0400, "Not implemented"};
 }
 
 // reserved error code (0x401) for reporting an invalid value.
 auto invalid_value() {
-  return alpaca_error(0x0401, "Invalid value");
+  return alpaca_error{0x0401, "Invalid value"};
 }
 
 // reserved error code (0x402) for reporting that a value has not been set.
 auto value_not_set() {
-  return alpaca_error(0x0402, "Value not set");
+  return alpaca_error{0x0402, "Value not set"};
 }
 
 // reserved error code (0x407) used to indicate that the communications
 // channel is not connected.
 auto not_connected() {
-  return alpaca_error(0x0407, "Not connected");
+  return alpaca_error{0x0407, "Not connected"};
 }
 
 // reserved error code (0x408) used to indicate that the attempted operation
 // is invalid because the mount is currently in a Parked state.
 auto parked() {
-  return alpaca_error(0x0408, "Parked");
+  return alpaca_error{0x0408, "Parked"};
 }
 
 // reserved error code (0x409) used to indicate that the attempted
 // operation is invalid because the mount is currently in a Slaved state.
 auto slaved() {
-  return alpaca_error(0x0409, "Slaved");
+  return alpaca_error{0x0409, "Slaved"};
 }
 
 // reserved error code (0x40B) to indicate that the requested
 // operation can not be undertaken at this time.
 auto invalid_operation() {
-  return alpaca_error(0x040B, "Invalid operation");
+  return alpaca_error{0x040B, "Invalid operation"};
 }
 
 // reserved error code (0x40C) to indicate that the requested
 // action is not implemented in this driver.
 auto action_not_implemented() {
-  return alpaca_error(0x040C, "Action not implemented");
-}
-
-auto custom_error(const std::string& str) {
-  return alpaca_error(0x500, str);
+  return alpaca_error{0x040C, "Action not implemented"};
 }
 
 // [0x500 - 0xfff] reserved for driver specific errors.
+auto custom_error(const std::string& str) {
+  return alpaca_error{0x500, str};
+}
 
 }  // namespace alpaca
 
